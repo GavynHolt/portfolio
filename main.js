5a720f5c57a9316be6dd1592db009e02b9a4d5fd
Gavyn Holt <gavynholt@gmail.com>
2021-08-11 11:18:58 -0400

    accessibility fixes

--- main.js
+++ main.js
@@ -52,4 +52,18 @@ document.addEventListener('DOMContentLoaded', () => {
       navClose.classList.add('hide');
     });
   }
+
+  // Handle Form Submit
+  const contactForm = document.querySelector('form');
+  contactForm.addEventListener('submit', handleSubmit);
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(contactForm);
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: new URLSearchParams(formData).toString(),
+    }).then(() => console.log('form successfully submitted'));
+  };
 });
